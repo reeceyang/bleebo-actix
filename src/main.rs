02aaf787910806f0f8a64db02bc7cc476f927524
Reece Yang <reeceyang@live.com>
2024-03-22 15:11:40 -0400

    add custom host route middleware

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,96 @@ use actix_web_httpauth::extractors::AuthenticationError;
 use actix_web_httpauth::middleware::HttpAuthentication;
 use futures_util::future::FutureExt;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use std::future::{ready, Ready};
+
+use actix_web::dev::{forward_ready, Service, ServiceResponse, Transform};
+use futures_util::future::LocalBoxFuture;
+
+// There are two steps in middleware processing.
+// 1. Middleware initialization, middleware factory gets called with
+//    next service in chain as parameter.
+// 2. Middleware's call method gets called with normal request.
+pub struct HostRoute;
+
+impl HostRoute {
+    pub fn new() -> Self {
+        HostRoute {}
+    }
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+// Middleware factory is `Transform` trait
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S, ServiceRequest> for HostRoute
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = HostRouteMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(HostRouteMiddleware { service }))
+    }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+pub struct HostRouteMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for HostRouteMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        println!("Hi from start. You requested: {}", req.path());
+
+        let host = req.headers().get("Host").unwrap().to_str().unwrap();
+
+        if host == BASE_HOST {
+            let fut = self.service.call(req);
+            return Box::pin(async move {
+                let res = fut.await?;
+
+                Ok(res)
+            });
+        }
+
+        let subdomain = host
+            .strip_suffix(BASE_HOST)
+            .unwrap()
+            .strip_suffix(".")
+            .unwrap();
+
+        let new_req = TestRequest::with_uri(format!("/site/{}/{}", subdomain, req.uri()).as_str())
+            .to_srv_request();
+
+        let fut = self.service.call(new_req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+
+            Ok(res)
+        })
+    }
 }
 
 const BASE_HOST: &str = "bleebo.reeceyang.xyz";
 
 // auth middleware
-async fn validator(
-    req: ServiceRequest,
-    credentials: BasicAuth,
-) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+async fn upload(req: ServiceRequest, credentials: BasicAuth) -> Result<ServiceRequest, Error> {
     let config = req
         .app_data::<Config>()
         .map(|data| Pin::new(data).get_ref().clone())
@@ -41,46 +110,31 @@ async fn validator(
     println!("{}", req.headers().get("Host").unwrap().to_str().unwrap());
     let host = req.headers().get("Host").unwrap().to_str().unwrap();
 
-    if host == BASE_HOST {
-        // TODO: validate credentials
-        println!(
-            "user_id {} password {:?}",
-            credentials.user_id(),
-            credentials.password()
-        );
-        // match auth::validate_token(credentials.token()) {
-        //     Ok(res) => {
-        //         if res == true {
-        //             Ok(req)
-        //         } else {
-        //             Err(AuthenticationError::from(config).into())
-        //         }
-        //     }
-        //     Err(_) => Err(AuthenticationError::from(config).into()),
-        // }
-        return Err((AuthenticationError::from(config).into(), req));
-    }
-
-    let subdomain = host
-        .strip_suffix(BASE_HOST)
-        .unwrap()
-        .strip_suffix(".")
-        .unwrap();
-
-    let new_req = TestRequest::with_uri(format!("/site/{}/{}", subdomain, req.uri()).as_str())
-        .to_srv_request();
-    Ok(new_req)
+    // TODO: validate credentials
+    println!(
+        "user_id {} password {:?}",
+        credentials.user_id(),
+        credentials.password()
+    );
+    // match auth::validate_token(credentials.token()) {
+    //     Ok(res) => {
+    //         if res == true {
+    //             Ok(req)
+    //         } else {
+    //             Err(AuthenticationError::from(config).into())
+    //         }
+    //     }
+    //     Err(_) => Err(AuthenticationError::from(config).into()),
+    // }
+    return Err(AuthenticationError::from(config).into());
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .wrap(HttpAuthentication::basic(validator))
+            .wrap(HostRoute::new())
             .service(actix_files::Files::new("/site", "site/").index_file("index.html"))
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
